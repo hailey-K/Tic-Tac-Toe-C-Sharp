05eaff7b9dde78cd6a42545fa46abe53b809a6b8
DESKTOP-CN8NUDP\RIM <ghl2682@gmail.com>
2017-12-02 21:40:55 -0500

    The completed version of the Tic-Tac-Toe Game

---
+++ HKimAssignment2/HKimAssignment2/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace HKimAssignment2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic9 = new System.Windows.Forms.PictureBox();
+            this.pic8 = new System.Windows.Forms.PictureBox();
+            this.pic3 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic6 = new System.Windows.Forms.PictureBox();
+            this.pic5 = new System.Windows.Forms.PictureBox();
+            this.pic4 = new System.Windows.Forms.PictureBox();
+            this.pic7 = new System.Windows.Forms.PictureBox();
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic9
+            // 
+            this.pic9.Image = global::HKimAssignment2.Properties.Resources.back_card;
+            this.pic9.Location = new System.Drawing.Point(495, 401);
+            this.pic9.Name = "pic9";
+            this.pic9.Size = new System.Drawing.Size(199, 158);
+            this.pic9.TabIndex = 18;
+            this.pic9.TabStop = false;
+            this.pic9.Click += new System.EventHandler(this.pic_Click);
+            // 
+            // pic8
+            // 
+            this.pic8.Image = global::HKimAssignment2.Properties.Resources.back_card;
+            this.pic8.Location = new System.Drawing.Point(273, 401);
+            this.pic8.Name = "pic8";
+            this.pic8.Size = new System.Drawing.Size(199, 158);
+            this.pic8.TabIndex = 17;
+            this.pic8.TabStop = false;
+            this.pic8.Click += new System.EventHandler(this.pic_Click);
+            // 
+            // pic3
+            // 
+            this.pic3.Image = global::HKimAssignment2.Properties.Resources.back_card;
+            this.pic3.Location = new System.Drawing.Point(495, 51);
+            this.pic3.Name = "pic3";
+            this.pic3.Size = new System.Drawing.Size(199, 158);
+            this.pic3.TabIndex = 16;
+            this.pic3.TabStop = false;
+            this.pic3.Click += new System.EventHandler(this.pic_Click);
+            // 
+            // pic2
+            // 
+            this.pic2.Image = global::HKimAssignment2.Properties.Resources.back_card;
+            this.pic2.Location = new System.Drawing.Point(273, 51);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(199, 158);
+            this.pic2.TabIndex = 15;
+            this.pic2.TabStop = false;
+            this.pic2.Click += new System.EventHandler(this.pic_Click);
+            // 
+            // pic6
+            // 
+            this.pic6.Image = global::HKimAssignment2.Properties.Resources.back_card;
+            this.pic6.Location = new System.Drawing.Point(495, 226);
+            this.pic6.Name = "pic6";
+            this.pic6.Size = new System.Drawing.Size(199, 158);
+            this.pic6.TabIndex = 14;
+            this.pic6.TabStop = false;
+            this.pic6.Click += new System.EventHandler(this.pic_Click);
+            // 
+            // pic5
+            // 
+            this.pic5.Image = global::HKimAssignment2.Properties.Resources.back_card;
+            this.pic5.Location = new System.Drawing.Point(273, 226);
+            this.pic5.Name = "pic5";
+            this.pic5.Size = new System.Drawing.Size(199, 158);
+            this.pic5.TabIndex = 13;
+            this.pic5.TabStop = false;
+            this.pic5.Click += new System.EventHandler(this.pic_Click);
+            // 
+            // pic4
+            // 
+            this.pic4.Image = global::HKimAssignment2.Properties.Resources.back_card;
+            this.pic4.Location = new System.Drawing.Point(52, 226);
+            this.pic4.Name = "pic4";
+            this.pic4.Size = new System.Drawing.Size(199, 158);
+            this.pic4.TabIndex = 12;
+            this.pic4.TabStop = false;
+            this.pic4.Click += new System.EventHandler(this.pic_Click);
+            // 
+            // pic7
+            // 
+            this.pic7.Image = global::HKimAssignment2.Properties.Resources.back_card;
+            this.pic7.Location = new System.Drawing.Point(52, 401);
+            this.pic7.Name = "pic7";
+            this.pic7.Size = new System.Drawing.Size(199, 158);
+            this.pic7.TabIndex = 11;
+            this.pic7.TabStop = false;
+            this.pic7.Click += new System.EventHandler(this.pic_Click);
+            // 
+            // pic1
+            // 
+            this.pic1.Image = global::HKimAssignment2.Properties.Resources.back_card;
+            this.pic1.Location = new System.Drawing.Point(52, 51);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(199, 158);
+            this.pic1.TabIndex = 10;
+            this.pic1.TabStop = false;
+            this.pic1.Click += new System.EventHandler(this.pic_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::HKimAssignment2.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(747, 612);
+            this.Controls.Add(this.pic9);
+            this.Controls.Add(this.pic8);
+            this.Controls.Add(this.pic3);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.pic6);
+            this.Controls.Add(this.pic5);
+            this.Controls.Add(this.pic4);
+            this.Controls.Add(this.pic7);
+            this.Controls.Add(this.pic1);
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe";
+            ((System.ComponentModel.ISupportInitialize)(this.pic9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.PictureBox pic7;
+        private System.Windows.Forms.PictureBox pic4;
+        private System.Windows.Forms.PictureBox pic5;
+        private System.Windows.Forms.PictureBox pic6;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.PictureBox pic3;
+        private System.Windows.Forms.PictureBox pic8;
+        private System.Windows.Forms.PictureBox pic9;
+    }
+}
+
---
+++ HKimAssignment2/HKimAssignment2/Form1.cs
@@ -0,0 +1,125 @@
+﻿/***************************************************************************
+ *  NAME : HYERIM KIM
+ *  STUDENT NUMBER : 7518301
+ *  REVISION HISTORY : OCT 4TH 2017
+ *  PROJECT : ASSIGNMENT 2
+ *  
+ *  
+ *  DOCUMENTATION COMMENT :
+ *  THIS IS TIC-TAC-TOE GAME. 
+ *  THE GAME ALWAYS START FROM X AND IT WILL SHOW MESSAGE IF YOU WIN OR DRAW.
+ *  AFTER COMPLETING THE GAME, IT WILL START NEW GAME AUTOMATICALLY.
+ ***************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HKimAssignment2
+{
+    public partial class Form1 : Form
+    {
+        //IMAGE VARIABLES 
+        static readonly Image backCard = Properties.Resources.back_card;
+        static readonly Image tictactoeO = Properties.Resources.tictactoeO;
+        static readonly Image tictactoeX = Properties.Resources.tictactoeX;
+        // VARIABLE THAT INDICATES WHOES TURN IS
+        static string turn = "tictactoeX";
+        PictureBox[,] imageData = new PictureBox[3, 3];
+        public Form1()
+        {
+            InitializeComponent();
+            imageData = new PictureBox[3, 3] { { pic1, pic2, pic3 }, { pic4, pic5, pic6 }, { pic7, pic8, pic9 } };
+        }
+        //IMAGE CLICK EVENT 
+        private void pic_Click(object sender, EventArgs e)
+        {
+            Image current = ((PictureBox)sender).Image;
+            if (backCard.Tag == current.Tag)
+            {
+                if (turn == "tictactoeX")
+                {
+                    ((PictureBox)sender).Image = Properties.Resources.tictactoeX;
+                    ((PictureBox)sender).Image.Tag = turn;
+                    WinnerCheck1(turn);
+                    turn = "tictactoeO";
+                }
+                else
+                {
+                    ((PictureBox)sender).Image = Properties.Resources.tictactoeO;
+                    ((PictureBox)sender).Image.Tag = turn;
+                    WinnerCheck1(turn);
+                    turn = "tictactoeX";
+                }
+            }
+        }
+        //CHECK WINNER 
+        private void WinnerCheck1(string turn)
+        {
+
+            bool winnerCheck = false;
+            bool blankCheck = false;
+            for (int i = 0; i < imageData.GetLength(0); i++)
+            {
+                if (mutipleEquals(imageData[i, 0].Image, imageData[i, 1].Image, imageData[i, 2].Image))
+                    winnerCheck = true;
+                if (imageData[i, 0].Image.Tag == backCard.Tag || imageData[i, 1].Image.Tag == backCard.Tag || imageData[i, 2].Image.Tag == backCard.Tag
+                 || imageData[i, 0].Image.Tag == null || imageData[i, 1].Image.Tag == null || imageData[i, 2].Image.Tag == null)
+                {
+                    blankCheck = true;
+                }
+
+            }
+            for (int j = 0; j < imageData.GetLength(1); j++)
+                if (mutipleEquals(imageData[0, j].Image, imageData[1, j].Image, imageData[2, j].Image))
+                    winnerCheck = true;
+
+            if (mutipleEquals(imageData[0, 0].Image, imageData[1, 1].Image, imageData[2, 2].Image) ||
+                mutipleEquals(imageData[2, 0].Image, imageData[1, 1].Image, imageData[0, 2].Image))
+            {
+                winnerCheck = true;
+            }
+            if (!blankCheck && winnerCheck == false)
+            {
+                MessageBox.Show("Draw!");
+                NewGameStart();
+            }
+            else if (winnerCheck)
+            {
+                MessageBox.Show(turn + " is Winner !");
+                NewGameStart();
+            }
+        }
+        // CHECK THREE PARAMETERS HAS THE SAME TAGS
+        private bool mutipleEquals(Image firstText, Image secondText, Image thirdText)
+        {
+            if (firstText.Tag == secondText.Tag && firstText.Tag == thirdText.Tag && secondText.Tag == thirdText.Tag
+                && firstText.Tag != backCard.Tag && secondText.Tag != backCard.Tag && thirdText.Tag != backCard.Tag
+                && firstText.Tag != null && secondText.Tag != null && thirdText.Tag != null)
+                return true;
+
+            else
+                return false;
+        }
+        //START NEW GAME (INITIALIZE MANUALLY)
+        private void NewGameStart()
+        {
+            for (int i = 0; i < imageData.GetLength(0); i++)
+            {
+                imageData[i, 0].Image.Tag = null;
+                imageData[i, 1].Image.Tag = null;
+                imageData[i, 2].Image.Tag = null;
+                imageData[i, 0].Image = backCard;
+                imageData[i, 1].Image = backCard;
+                imageData[i, 2].Image = backCard;
+            }
+            turn = "tictactoeX";
+        }
+    }
+}
